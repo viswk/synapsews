7b27665d891b910b9dedbe9b62186c0c7221896b
Vis <viswajithkrishnas@gmail.com>
2023-08-30 14:08:51 +0100

    snow_sql_changes

---
+++ snow.sql
@@ -0,0 +1,16 @@
+--create table viswk_db.public.newtablesnow(id int,name varchar(20), location varchar(20))
+
+
+insert into viswk_db.public.newtablesnow
+select 1000,'viswajith','India'
+union all
+select 2000,'visw','uk'
+union all
+select 3000,'jith','US'
+union all
+select 4000,'th','UAE';
+
+
+select * from viswk_db.public.newtablesnow
+
+
