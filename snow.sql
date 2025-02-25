4ed615120d1254b5c35f6860af679327e3351cbd
viswk <139757428+viswk@users.noreply.github.com>
2025-02-25 14:25:01 +0000

    Update snow.sql

--- snow.sql
+++ snow.sql
@@ -63,3 +63,16 @@ select 3000,'jith','US'
 union all
 select 4000,'th','UAE';
 
+insert into viswk_db.public.newtablesf1changes
+select 1000,'viswajith','India'
+union all
+select 2000,'visw','uk'
+union all
+select 3000,'jith','US'
+union all
+select 4000,'th','UAE';
+
+select * from viswk_db.public.newtablesnow3; 
+
+
+
